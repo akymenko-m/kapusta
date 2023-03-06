ce8e8222141a2f1f6fd98c6f5d4292ca513a5392
YuliiaGagina <ygaginazy@gmail.com>
2023-03-06 15:26:46 +0200

    add base name for github

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Provider> */}
      <Global styles={GlobalStyles} />
-     <BrowserRouter>
+     <BrowserRouter  basename='/kapusta/'>
     <App />
     </BrowserRouter>
     {/* </Provider> */}
