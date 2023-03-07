129db02390f551e00901d59c7b5bb9f696908a95
YuliiaGagina <ygaginazy@gmail.com>
2023-03-07 15:38:11 +0200

    add changes to index js

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,23 +1,18 @@
 import { NavLink } from 'react-router-dom';
-import logo from "../../images/logo/logo.png";
-import { Logout } from '../Logout'
+import logo from '../../images/logo/logo.png';
+import { Logout } from '../Logout';
 import { useAuth } from '../../hook/useAuth';
 
-
 export const UserMenu = () => {
-   const { isLoggedIn } = useAuth();
+  const { isLoggedIn } = useAuth();
   return (
     <>
-
       <NavLink to="/">{<img src={logo} alt="logo" />}</NavLink>
       {isLoggedIn && <Logout />}
 
+      <NavLink to="/">{<img src={logo} alt="logo" />}</NavLink>
 
-      <NavLink to="/">{<img src={logo} alt="logo"/>}</NavLink>
-      
-        <p>Welcome, User</p>
-
-
+      <p>Welcome, User</p>
 
       <NavLink to="/transaction/period-data">Reports</NavLink>
 
