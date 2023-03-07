9bfaed150e10d2e2ff7ac9ebf33ceee4162ee9a3
Nadin Severin <nadinlikes2018@gmail.com>
2023-03-08 01:22:46 +0200

    fixed_calendar

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { DatePickerStyled } from './DatePicker.styled';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const Example = ({ onChange }) => {
   const [startDate, setStartDate] = useState(new Date());
+
+  useEffect(() => {
+    onChange(startDate);
+  }, []);
+
   const getStartDate = date => {
     setStartDate(date);
     onChange(date);
--- src/index.js
+++ src/index.js
@@ -17,23 +17,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <PersistGate loading={null} persistor={persistor}>
         <Global styles={GlobalStyles} />
 
-
-
-
-
-
-          <BrowserRouter      basename="/kapusta/"   >
-       
-         
-
-
-
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
-
-
+// basename = '/kapusta/';
