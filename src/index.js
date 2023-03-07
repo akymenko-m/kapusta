7b82d580326ac96b04892c7dbf2c8a634d82bc5c
YuliiaGagina <ygaginazy@gmail.com>
2023-03-07 13:53:35 +0200

    add tabs

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Global styles={GlobalStyles} />
-          <BrowserRouter  basename="/kapusta/" >
-       
+          <BrowserRouter  >
+          {/* basename="/kapusta/" */}
           <App />
         </BrowserRouter>
       </PersistGate>
