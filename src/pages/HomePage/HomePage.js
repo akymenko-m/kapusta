7a449a3670fef90722b5632238a80125cf3b7035
YuliiaGagina <ygaginazy@gmail.com>
2023-03-08 23:14:44 +0200

    add private and resticted route

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,4 +1,10 @@
-import {  Title, Wrapper, Background, CabbageTop, CabbageBottom } from './HomePage.styled';
+import {
+  Title,
+  Wrapper,
+  Background,
+  CabbageTop,
+  CabbageBottom,
+} from './HomePage.styled';
 import title from '../../images/title/title.png';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
@@ -14,56 +20,56 @@ import { Outlet } from 'react-router-dom';
 import { useAuth } from '../../hook/useAuth';
 import { Summary } from 'components/Summary/Summary';
 
-
-
-
-
 export const HomePage = () => {
-    const { isLoggedIn } = useAuth();
-  const location =useLocation();
- 
-const index = location.pathname === '/income'?1 : 0;
+  const { isLoggedIn } = useAuth();
+  const location = useLocation();
 
-    return (
-      <>
-      {isLoggedIn ? (<Background><Container>  <div>
-                 
-                 <Balance/>
-                 <Tabs selectedIndex={index} onSelect={()=>{}}>
-                   <TabList>
-                     <Tab>
-                       <NavLink  to="expenses">
-                         Expenses
-                       </NavLink>
-                     </Tab>
-                     <Tab >
-                   <NavLink to="income">
-                         Income
-                       </NavLink>
-                     </Tab>
-                   </TabList>
-                   <TabPanel >{<Outlet />}</TabPanel>
-                   <TabPanel >{<Outlet />}</TabPanel>
-                  
-                 </Tabs>
+  const index = location.pathname === '/income' ? 1 : 0;
 
-               </div></Container><CabbageBottom/></Background>) : (<Background><CabbageTop><Container>    <Wrapper>
-                  <div>
-                    <Title src={title} />
-                  </div>
-                  <div>
-                    <LoginPage />
-                    <RegisterPage />
-                  </div>
-                </Wrapper><FooterOfApp /></Container></CabbageTop></Background>)}
-                                                         <Summary />
-       
-      </>
-    );
-  };
-  
-
-
-
-
-
+  return (
+    <>
+      {isLoggedIn ? (
+        <Background>
+          <Container>
+            {' '}
+            <div>
+              <Balance />
+              <Tabs selectedIndex={index} onSelect={() => {}}>
+                <TabList>
+                  <Tab>
+                    <NavLink to="expenses">Expenses</NavLink>
+                  </Tab>
+                  <Tab>
+                    <NavLink to="income">Income</NavLink>
+                  </Tab>
+                </TabList>
+                <TabPanel>{<Outlet />}</TabPanel>
+                <TabPanel>{<Outlet />}</TabPanel>
+              </Tabs>
+            </div>
+          </Container>
+          <CabbageBottom />
+        </Background>
+      ) : (
+        <Background>
+          <CabbageTop>
+            <Container>
+              {' '}
+              <Wrapper>
+                <div>
+                  <Title src={title} />
+                </div>
+                <div>
+                  <LoginPage />
+                  <RegisterPage />
+                </div>
+              </Wrapper>
+              <FooterOfApp />
+            </Container>
+          </CabbageTop>
+        </Background>
+      )}
+      <Summary />
+    </>
+  );
+};
