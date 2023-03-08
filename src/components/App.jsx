011049bd4e05856a30378a4ba96643428f124291
YuliiaGagina <ygaginazy@gmail.com>
2023-03-08 12:16:22 +0200

    add outlet for tabs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,6 @@ import { refreshUser } from 'redux/user/userOperations';
 import { ReportsPage } from 'pages/ReportsPage/ReportsPage';
 import { FooterOfApp } from './Footer/Footer';
 
-
 export const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -18,18 +17,17 @@ export const App = () => {
 
   return (
     <>
+      <Header />
       <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<HomePage />} />
+        <Route path="/" element={<HomePage />}>
           <Route path="expenses" element={<Expenses />} />
           <Route path="income" element={<Income />} />
-          <Route path="/transaction/period-data" element={<ReportsPage />} />
         </Route>
 
-        <Route element={ <FooterOfApp/>}/>
+        <Route path="/transaction/period-data" element={<ReportsPage />} />
+
+        <Route element={<FooterOfApp />} />
         <Route path="*" element={<Navigate to="/" replace={true} />} />
-
-
       </Routes>
     </>
   );
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,21 +4,18 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { RegisterPage } from 'pages/RegisterPage';
 import { LoginPage } from '../LoginPage/LoginPage';
-import { Expenses } from 'components/Expenses/Expenses';
-import { Income } from 'components/Income/Income';
+// import { Expenses } from 'components/Expenses/Expenses';
+// import { Income } from 'components/Income/Income';
 import { FooterOfApp } from 'components/Footer/Footer';
 import { Container } from 'components/App.styled';
 import { Balance } from 'components/Balance/Balance';
 import { NavLink } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
 import { useAuth } from '../../hook/useAuth';
 
 export const HomePage = () => {
-  const location = useLocation();
-//   const goBackLink = location?.state?.from ?? '/';
   const { isLoggedIn } = useAuth();
-  console.log(location);
+
 
   return (
     <>
@@ -32,21 +29,22 @@ export const HomePage = () => {
                 <Tabs>
                   <TabList>
                     <Tab>
-                      <NavLink state={{ from: location }} to="expenses">
+                      <NavLink  to="expenses">
                         Expenses
                       </NavLink>
                     </Tab>
                     <Tab>
                   
-                      <NavLink state={{ from: location }} to="income">
+                      <NavLink to="income">
                         Income
                       </NavLink>
                     </Tab>
                   </TabList>
-                  <TabPanel>{<Expenses />}</TabPanel>
-                  <TabPanel>{<Income />}</TabPanel>
+                  <TabPanel >{<Outlet />}</TabPanel>
+                  <TabPanel >{<Outlet />}</TabPanel>
+                 
                 </Tabs>
-                <Outlet/>
+            
               </div>
             ) : (
               <Wrapper>
